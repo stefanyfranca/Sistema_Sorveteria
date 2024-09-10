bdd10c247a3a3dd6dbcb99ead38bd958ce6283b9
Stefany França <133697317+stefanyfranca@users.noreply.github.com>
2024-09-10 15:10:11 -0300

    estilização completa login

--- login.php
+++ login.php
@@ -1,97 +1,182 @@
-<html>
-    <head>
-        <title>Login Usuarios</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css" >
-  <style type="text/css">
-      body{
-        margin-top: 20%;
-        text-align: center;
-        background-color: #C6e2e9;
-
-      }
-      
-      .form-controle {
-        display: block;
-        width: 35%; 
-        height: 60px;
-        padding: 6px 12px;
-        font-size: 14px;
-        line-height: 1.42857143;
-        color: #555;
-        background-color: #fff;
-        background-image: none;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
-        box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
-        -webkit-transition: border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;
-        -o-transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
-        transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
-            margin-left: 35px;
-       }
-       label {
-        display: inline-block;
-        max-width: 100%;
-        margin-bottom: 5px;
-        font-weight: 700;
-        margin-left: 35px;
-        }
-        .h1, h1 {
-        font-size: 36px;
-        margin-left: 35px;
-        }
-        select{
-            width: 200px;
-            padding: 10px;
-            font-size: 12px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            background-color: #fff;
-            color: #333;
-        }
-
-        select option{
-            background-color: #fff;
-            color: #333;
-        }
-    
-  </style>
-    </head>
-    <body>
-        <h2>Login do Usuario: </h2>
-        <form name="formulario" method="post" action="login.php">
-            <label>cpf:</label>
-            <input type="text" name="cpf" id="cpf" size=20 required>
-            <label>senha:</label>
-            <input type="password" name="senha" id="senha" size=20 required>
-            <br><br>
-            <input type="submit" value="Conectar" id="conectar" name="conectar">
-        </form>
-    </body>
-</html>
-
 <?php
-$conectar = mysql_connect('localhost','root','');
-$banco    = mysql_select_db("frangelato");
+session_start(); // Iniciar a sessão antes de qualquer saída
 
-if (isset($_POST['conectar']))
-{
+// Conectar ao banco de dados com mysqli
+$conexao = new mysqli('localhost', 'root', '', 'frangelato');
+
+if ($conexao->connect_error) {
+    die("Conexão falhou: " . $conexao->connect_error);
+}
+
+$mensagem_erro = '';
+
+if (isset($_POST['conectar'])) {
     $cpf = $_POST['cpf'];
     $senha = $_POST['senha'];
 
-    $sql = mysql_query("select * FROM usuario where cpf='$cpf' and senha='$senha'");
+    // Usar prepared statements para evitar SQL injection
+    $stmt = $conexao->prepare("SELECT * FROM usuario WHERE cpf = ? AND senha = ?");
+    $stmt->bind_param("ss", $cpf, $senha); // 'ss' significa dois parâmetros do tipo string
+    $stmt->execute();
+    $resultado = $stmt->get_result();
 
-    $resultado = mysql_num_rows($sql);
-
-    if ($resultado == 0)
-    {
-        echo "Login ou senha invalido...";
-    }
-    else
-    {
-        session_start();
+    if ($resultado->num_rows == 0) {
+        // Se o login falhar, exibe a mensagem de erro
+        $mensagem_erro = "Login ou senha inválido...";
+    } else {
         $_SESSION['cpf'] = $cpf;
-        header("Location:menucadastros.html");
+        header("Location: screens/widgets/widgets.php");
+        exit(); // Importante para evitar que o script continue após o redirecionamento
     }
+
+    $stmt->close();
 }
+
+$conexao->close();
 ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login Usuários</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style type="text/css">
+            body {
+                margin: 0;
+                padding: 0;
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-color: #C6e2e9;
+            }
+
+            .container {
+                display: flex;
+                width: 100%;
+                height: 100%;
+                box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+            }
+
+            .left-section {
+                background-color: #6B0000;
+                width: 33%;
+                display: flex;
+                justify-content: flex-end;
+                align-items: center;
+                position: relative;
+            }
+
+            .right-section {
+                background-color: #ffffff;
+                width: 67%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding: 40px;
+                position: relative;
+            }
+
+            .logo {
+                position: absolute;
+                left: calc(33% - 300px);
+                top: 50%;
+                transform: translateY(-50%);
+            }
+
+            .logo img {
+                width: 550px;
+                height: auto;
+            }
+
+            .right-section h2 {
+                margin-bottom: 30px;
+                font-size: 24px;
+            }
+
+            .form-controle {
+                width: 100%;
+                height: 40px;
+                padding: 6px 12px;
+                margin-bottom: 25px;
+                font-size: 14px;
+                line-height: 1.42857143;
+                color: #8D8C8C;
+                background-color: #fff;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+            }
+
+            .btn-login {
+                width: 150px;
+                padding: 10px;
+                background-color: #6B0000;
+                color: white;
+                border: none;
+                border-radius: 5px;
+                cursor: pointer;
+                text-align: center;
+            }
+
+            .btn-login:hover {
+                background-color: #440000;
+            }
+
+            .formulario {
+                margin-left: 400px;
+                align-items: center;
+                margin-top: 50px;
+                width: 40%;
+                font-family: "Gill Sans", sans-serif;
+                color: #8D8C8C;
+            }
+
+            label {
+                font-size: 14px;
+                margin-bottom: 5px;
+                display: block;
+            }
+
+            .btn-container {
+                text-align: center;
+            }
+
+            .error-message {
+                color: red;
+                font-size: 14px;
+                margin-bottom: 15px;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="left-section"></div>
+
+            <div class="right-section">
+                <div class="formulario">
+                    <form name="formulario" method="post" action="">
+                        <label for="cpf">CPF:</label>
+                        <input type="text" class="form-controle" name="cpf" id="cpf" placeholder="Digite seu CPF" required>
+
+                        <label for="senha">Senha:</label>
+                        <input type="password" class="form-controle" name="senha" id="senha" placeholder="Digite sua senha" required>
+
+                        <?php if (!empty($mensagem_erro)): ?>
+                            <div class="error-message">
+                                <?php echo $mensagem_erro; ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <div class="btn-container">
+                            <input type="submit" class="btn-login" value="Conectar" id="conectar" name="conectar">
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="logo">
+                <img src="logo.png" alt="Logo da Empresa" height="200" width="200">
+            </div>
+        </div>
+    </body>
+</html>
---
+++ screens/widgets/widgets.php
@@ -0,0 +1,14 @@
+<?php
+$conectar = mysql_connect('localhost','root','');
+$db       = mysql_select_db('frangelato');
+?>
+<html>
+
+<head>
+</head>
+
+<body>
+oiii
+</body>
+
+</html>
